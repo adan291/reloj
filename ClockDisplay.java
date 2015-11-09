25b9328ddd030154926eb48361df66ae5e79c03c
adan291 <adanx05@gmail.com>
2015-11-09 13:44:40 +0100

    Creado reloj con inicializador de tiempo y con mostrado y suma de horas, min y seg

---
+++ ClockDisplay.java
@@ -0,0 +1,68 @@
+
+/**
+ * Write a description of class Reloj here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    private int x;
+    //Crear horas
+    private int h;
+    //Crear minutos
+    private int m;
+    //Crear segundos
+    private int s;
+
+    
+    /**
+     * Elejir tiempo
+     */
+    public void SetTime(int hh, int mm, int ss)
+    {
+        // Damos horas, minutos y segundos
+        h=hh;
+        m=mm;
+        s=ss;
+        
+    }
+    
+    /**
+     * uno mas al timepo
+     */
+    public void timeTick()
+    {
+        if (m>59){
+            h=h+1;
+            m=0;
+        }
+        else if(s<59){
+            m=m+1;
+            s=0;
+        }
+    }
+    
+    /**
+     * Mostrar tiempo
+     */
+    public String getTime()
+    {
+       String horAc = "%02d:%02d";
+       String resultado = String.format(horAc,h,m,s);
+       return resultado;
+  
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
